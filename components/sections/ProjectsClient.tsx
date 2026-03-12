dbccfa40dc09b229063e856b3ece5910b484f762
Kiboxglx <kiboxglx@users.noreply.github.com>
2026-03-12 12:07:22 -0300

    Fix: build error in ProjectsClient due to removed prop

--- components/sections/ProjectsClient.tsx
+++ components/sections/ProjectsClient.tsx
@@ -1,9 +1,7 @@
 "use client"
 
 import { Projects } from "@/components/sections/Projects"
-import { useModal } from "@/lib/modal-context"
 
 export const ProjectsClient = () => {
-    const { openModal } = useModal()
-    return <Projects onEstimateClick={openModal} />
+    return <Projects />
 }
